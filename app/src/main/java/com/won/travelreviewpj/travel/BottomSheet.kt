0106ef158e9373bcdac6ab7ecec3fdde517c5f91
wonhyeung <whl0409@naver.com>
2023-11-11 18:42:58 +0900

    Feat: bottomSheet setUp

---
+++ app/src/main/java/com/won/travelreviewpj/travel/BottomSheet.kt
@@ -0,0 +1,19 @@
+package com.won.travelreviewpj.travel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.won.travelreviewpj.R
+
+class BottomSheet: BottomSheetDialogFragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_travel_bottomsheet,container,false)
+    }
+}
