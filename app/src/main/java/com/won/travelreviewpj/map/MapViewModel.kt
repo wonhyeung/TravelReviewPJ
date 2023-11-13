174a9661715f0ee49e4bc73e0c4544f81e3c5096
wonhyeung <whl0409@naver.com>
2023-11-13 13:43:17 +0900

    Feat: Fragment ViewModel SetUp

---
+++ app/src/main/java/com/won/travelreviewpj/map/MapViewModel.kt
@@ -0,0 +1,7 @@
+package com.won.travelreviewpj.map
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MapViewModel(application: Application) : AndroidViewModel(application) {
+}
