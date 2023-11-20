1d288e8fec1453dc5a7dfd99e2d151587c5ce5eb
wonhyeung <whl0409@naver.com>
2023-11-20 16:19:13 +0900

    Refactor: RecyclerView 설정

--- app/src/main/java/com/won/travelreviewpj/travel/wishlist/TravelWishlistFragment.kt
+++ app/src/main/java/com/won/travelreviewpj/travel/wishlist/TravelWishlistFragment.kt
@@ -1,27 +1,23 @@
 package com.won.travelreviewpj.travel.wishlist
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.won.travelreviewpj.R
 import com.won.travelreviewpj.common.ViewBindingBaseFragment
 import com.won.travelreviewpj.databinding.FragmentTravelWishlistBinding
+import com.won.travelreviewpj.travel.TravelEntity
+import com.won.travelreviewpj.travel.TravelRepository
 
 
 class TravelWishlistFragment :
     ViewBindingBaseFragment<FragmentTravelWishlistBinding>(FragmentTravelWishlistBinding::inflate) {
 
-    private val viewModel: TravelWishlistViewModel by viewModels()
-
-    private fun item() = mutableListOf<TravelWishlist>().apply {
-        add(TravelWishlist(R.drawable.travel_sample, "111", "111"))
-        add(TravelWishlist(R.drawable.travel_sample, "112", "112"))
-        add(TravelWishlist(R.drawable.travel_sample, "113", "113"))
-        add(TravelWishlist(R.drawable.travel_sample, "114", "114"))
-        add(TravelWishlist(R.drawable.travel_sample, "115", "115"))
-    }
-
+    private val wishListViewModel: TravelWishlistViewModel by viewModels()
+    private val adapter = TravelWishlistAdapter(mutableListOf())
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -30,7 +26,11 @@ class TravelWishlistFragment :
         )
         with(binding) {
             rvTravelWishlist.layoutManager = manager
-            rvTravelWishlist.adapter = TravelWishlistAdapter(item())
+            rvTravelWishlist.adapter = adapter
+        }
+        wishListViewModel.getAllTravelWishlist()?.observe(viewLifecycleOwner) { travelWishlists ->
+            adapter.setTravelWishList(travelWishlists)
+
         }
     }
 
